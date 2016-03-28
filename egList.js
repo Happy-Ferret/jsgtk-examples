b4b7ed9938567f4cd6e49e412325a4bd3b188379
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2016-03-28 14:20:37 +0200

    progress

--- egList.js
+++ egList.js
@@ -1,54 +1,34 @@
-#!/usr/bin/gjs
+#!/usr/bin/env jsgtk
 
 /*
-GJS example showing how to build Gtk javascript applications
+JSGtk+ example showing how to build Gtk javascript applications
 using Gtk TreeView and ListStore
 
 Run it with:
-    gjs egList.js
+    jsgtk egList.js
 */
 
-const Gio   = imports.gi.Gio;
-const GLib  = imports.gi.GLib;
 const GObj  = imports.gi.GObject;
 const Gtk   = imports.gi.Gtk;
 const Lang  = imports.lang;
 
-// Get application folder and add it into the imports path
-function getAppFileInfo() {
-    let stack = (new Error()).stack,
-        stackLine = stack.split('\n')[1],
-        coincidence, path, file;
-
-    if (!stackLine) throw new Error('Could not find current file (1)');
-
-    coincidence = new RegExp('@(.+):\\d+').exec(stackLine);
-    if (!coincidence) throw new Error('Could not find current file (2)');
-
-    path = coincidence[1];
-    file = Gio.File.new_for_path(path);
-    return [file.get_path(), file.get_parent().get_path(), file.get_basename()];
-}
-const path = getAppFileInfo()[1];
-imports.searchPath.push(path);
-
 const App = function () { 
 
     this.title = 'Example List';
-    GLib.set_prgname(this.title);
+    require('GLib').setPrgname(this.title);
 };
 
 App.prototype.run = function (ARGV) {
 
     this.application = new Gtk.Application();
-    this.application.connect('activate', Lang.bind(this, this.onActivate));
-    this.application.connect('startup', Lang.bind(this, this.onStartup));
+    this.application.connect('activate', this.onActivate.bind(this));
+    this.application.connect('startup', this.onStartup.bind(this));
     this.application.run([]);
 };
 
 App.prototype.onActivate = function () {
 
-    this.window.show_all();
+    this.window.showAll();
 };
 
 App.prototype.onStartup = function() {
@@ -59,23 +39,23 @@ App.prototype.onStartup = function() {
 App.prototype.buildUI = function() {
 
     this.window = new Gtk.ApplicationWindow({ application: this.application,
-                                              default_height: 300,
-                                              default_width: 720,
-                                              window_position: Gtk.WindowPosition.CENTER });
+                                              defaultHeight: 300,
+                                              defaultWidth: 720,
+                                              windowPosition: Gtk.WindowPosition.CENTER });
     try {
-        this.window.set_icon_from_file(path + '/assets/appIcon.png');
+        this.window.setIconFromFile(__dirname + '/assets/appIcon.png');
     } catch (err) {
-        this.window.set_icon_name('application-x-executable');
+        this.window.setIconName('application-x-executable');
     }
 
-    this.window.set_titlebar(this.getHeader());
+    this.window.setTitlebar(this.getHeader());
     this.window.add(this.getBody());
 };
 
 App.prototype.getHeader = function () {
 
     this.headerBar = new Gtk.HeaderBar();
-    this.headerBar.set_show_close_button(true);
+    this.headerBar.setShowCloseButton(true);
     return this.headerBar;
 };
 
@@ -86,38 +66,38 @@ App.prototype.getBody = function () {
     scroll = new Gtk.ScrolledWindow({ vexpand: true });
 
     store = new Gtk.ListStore();
-    store.set_column_types([GObj.TYPE_INT, GObj.TYPE_STRING, GObj.TYPE_STRING, GObj.TYPE_BOOLEAN]);
+    store.setColumnTypes([GObj.TYPE_INT, GObj.TYPE_STRING, GObj.TYPE_STRING, GObj.TYPE_BOOLEAN]);
     store.set(store.append(), [0, 1, 2, 3], [0, '0A', 'Name 0', false]);
     store.set(store.append(), [0, 1, 2, 3], [1, '1B', 'Name 1', false]);
     store.set(store.append(), [0, 1, 2, 3], [2, '2C', 'Name 2', false]);
     store.set(store.append(), [0, 1, 2, 3], [3, '3D', 'Name 3', false]);
 
-    tree = new Gtk.TreeView({ headers_visible: false, vexpand: true, hexpand: true });
-    tree.set_model(store);  
+    tree = new Gtk.TreeView({ headersVisible: false, vexpand: true, hexpand: true });
+    tree.setModel(store);
     scroll.add(tree);
 
     col = new Gtk.TreeViewColumn();
-    tree.append_column(col);
+    tree.appendColumn(col);
 
     let text1 = new Gtk.CellRendererText();
-    col.pack_start(text1, true);
-    col.set_cell_data_func(text1, Lang.bind(this, this.cellFuncText1));
+    col.packStart(text1, true);
+    col.setCellDataFunc(text1, this.cellFuncText1.bind(this));
 
     let text2 = new Gtk.CellRendererText();
-    col.pack_start(text2, true);
-    col.set_cell_data_func(text2, Lang.bind(this, this.cellFuncText2));
+    col.packStart(text2, true);
+    col.setCellDataFunc(text2, this.cellFuncText2.bind(this));
 
     return scroll;
 };
 
 App.prototype.cellFuncText1 = function (col, cell, model, iter) {
     cell.editable = false;
-    cell.text = model.get_value(iter, 1);
+    cell.text = model.getValue(iter, 1);
 };
 
 App.prototype.cellFuncText2 = function (col, cell, model, iter) {
     cell.editable = false;
-    cell.text = model.get_value(iter, 2);
+    cell.text = model.getValue(iter, 2);
 };
 
 //Run the application
